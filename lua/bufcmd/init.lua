2e7b53293b095d599aaf1a76e51a217dcd55cfa5
jradam <15231996+jradam@users.noreply.github.com>
2024-02-01 17:44:49 +0000

    added TODO

--- lua/bufcmd/init.lua
+++ lua/bufcmd/init.lua
@@ -15,6 +15,7 @@ local h = require("bufcmd.helpers")
 -- optional sorting (alphabetically, time open)
 -- use some default highlight groups so the user doesn't have to set up themselves
 -- find a programmatic way to determine command line available characters
+-- create a gif for the readme
 
 local function bufcmd(sets)
   local bufcmd_table = h.fetch_all_buffers(sets)
